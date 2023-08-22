2cdb0cf8472c52335979537b8baaaf281fe51677
aymanijaz <aymanijaz21@gmail.com>
2023-08-22 10:05:15 +0500

    Aggregate | Scalar | String | Date | Time

--- 10_Aggregate_Scalar_String_DateTime.sql
+++ 10_Aggregate_Scalar_String_DateTime.sql
@@ -34,34 +34,38 @@ select ROUND(COST) from PETRESCUE;
 select LENGTH(ANIMAL) from PETRESCUE;
 
 
-Query B3: Enter a function that displays the animal name in each rescue in uppercase.
+-- Query B3: Enter a function that displays the animal name in each rescue in uppercase.
+select UCASE(ANIMAL) from PETRESCUE;
+SELECT UPPER(ANIMAL) FROM `petrescue`; --works both of them
 
-Query B4: Enter a function that displays the animal name in each rescue in uppercase without duplications.
+-- Query B4: Enter a function that displays the animal name in each rescue in uppercase without duplications.
+select DISTINCT(UCASE(ANIMAL)) from PETRESCUE;
 
-Query B5: Enter a query that displays all the columns from the PETRESCUE table, where the animal(s) rescued are cats. Use cat in lower case in the query.
+-- Query B5: Enter a query that displays all the columns from the PETRESCUE table, where the animal(s) rescued are cats. Use cat in lower case in the query.
+
+-- step-1   all colums of cat 
+select * from PETRESCUE 
+WHERE ANIMAL = 'cat';
+
+-- Use cat in lower case in the query.
+select * from PETRESCUE WHERE LCASE(ANIMAL) = 'cat';
 
 
 ---------------------- Exercise 4: Date and Time Functions ----------------------
-Query C1: Enter a function that displays the day of the month when cats have been rescued.
 
-Query C2: Enter a function that displays the number of rescues on the 5th month.
-
-Query C3: Enter a function that displays the number of rescues on the 14th day of the month.
-
-Query C4: Animals rescued should see the vet within three days of arrivals. Enter a function that displays the third day from each rescue.
-
-Query C5: Enter a function that displays the length of time the animals have been rescued; the difference between today’s date and the rescue date.
+-- Query C1: Enter a function that displays the day of the month when cats have been rescued.
+SELECT DAY(RESCUEDATE) from PETRESCUE WHERE ANIMAL = 'cat';
 
 
+-- Query C2: Enter a function that displays the number of rescues on the 5th month.
+SELECT SUM(Quantity) from PETRESCUE WHERE MONTH(RESCUEDATE) = '05';
+
+-- Query C3: Enter a function that displays the number of rescues on the 14th day of the month.
+SELECT SUM(Quantity) from PETRESCUE WHERE DAY(RESCUEDATE) = '14';
+
+-- Query C4: Animals rescued should see the vet within three days of arrivals. Enter a function that displays the third day from each rescue.
+select DATE_add(RESCUEDATE, INTERVAL 3 DAY) from PETRESCUE;
 
 
-
-
-
-
-
-
-
-
-
-
+-- Query C5: Enter a function that displays the length of time the animals have been rescued; the difference between today’s date and the rescue date.
+select DATEDIFF(CURRENT_TIMESTAMP,RESCUEDATE) from PETRESCUE;
